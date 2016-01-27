2a92270941b282ff6096f61558eba937e51701c5
Chris Schmieder <ckschmieder@gmail.com>
2016-01-26 22:52:15 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package simone
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="">
+            <div>
+            	<h2>This is the Footer area.</h2>
+            </div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
